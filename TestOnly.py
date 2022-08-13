589f67f300695136bcb6c531d6dba78a73ed07dd
christelc2 <73996021+christelc2@users.noreply.github.com>
2022-08-13 21:06:15 +0930

    Add files via upload

---
+++ TestOnly.py
@@ -0,0 +1,17 @@
+'''
+Created on 4 June 2022
+
+@author: christelcherryadi
+'''
+#This command is just a sample
+print('[Game Master: To start, type your name]')
+name=input()
+print('Hi!', name, 'nice to meet you.')
+print('[Game Master: What is your top 3 favourites? Type in 3 separate lines]')
+name1, name2, name3=input(), input(), input()
+print('Amazing! So you like:', name1,',', name2, ',', name3)
+age=2022-1989
+print('I am guessing you are', age, 'years old.')
+a=1
+b=a+a+a
+print('Do you have', b, 'siblings?')
